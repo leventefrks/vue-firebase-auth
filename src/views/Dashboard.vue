894bd403424e92e5e0b0ad4d6d1c073bd2d14c6b
x-farkasle <x-farkasle@wizzair.com>
2021-03-22 19:17:47 +0100

    Dashboard - signout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,30 @@
 <template lang="html">
-  <h1>Dashboard</h1>
+  <div>
+    <h1>Dashboard</h1>
+    <button
+      class="block text-center text-white bg-gray-800 p-3 duration-300 rounded-sm hover:bg-black w-full"
+      @click="signOut"
+    >
+      Sign out
+    </button>
+  </div>
 </template>
 
 <script>
-import firebase from 'firebase';
+import firebase from "firebase";
+
 export default {
   name: "Dashboard",
 
   created() {
     console.log(firebase.getCurrentUser());
-  }
+  },
+
+  methods: {
+    signOut() {
+      firebase.auth().signOut();
+      this.$router.push('/');
+    },
+  },
 };
 </script>
